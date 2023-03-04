0766b745789c9b5482ca2a83d8ce3bfde5763f09
Daniel Krsak <krsak.daniel@gmail.com>
2023-03-04 15:33:17 +0100

    ...

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-"TEST";
+const express = require("express");
+const path = require("path");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json({ limit: "10kb" }));
+
+app.get("/api/v1/tours");
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Couldn't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+const appError = require("../utils/appError");
+
+dotenv.config({ path: "../config.env" });
+
+const errorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const errorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: "Something went wrong!",
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") errorDev(err, res);
+  if (process.env.NODE_ENV === "production") errorProd(err, res);
+};
