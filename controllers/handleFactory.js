d068831e4e6d17140795a75617d96d17b0fde8e3
Daniel Krsak <krsak.daniel@gmail.com>
2023-03-05 19:56:47 +0100

    Day 2

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,8 +1,15 @@
 const catchAsync = require("../utils/catchAsync");
+const AppFeatures = require("../utils/appFeatures");
 
 exports.getAll = (Model) => {
   return catchAsync(async (req, res, next) => {
-    const doc = await Model.find();
+    const features = new AppFeatures(Model, req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+
+    const doc = await features.query;
 
     res.status(200).json({
       status: "success",
---
+++ utils/appFeatures.js
@@ -0,0 +1,56 @@
+class AppFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["sort", "page", "fields", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const queryStr = JSON.stringify(queryObj).replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      (match) => `$${match}`
+    );
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-duration -price");
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select(
+        "name duration price ratingsAverage difficulty"
+      );
+    }
+
+    return this;
+  }
+
+  pagination() {
+    const page = this.queryString.page || 1;
+    const limit = this.queryString.limit || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.limit(limit).skip(skip);
+
+    return this;
+  }
+}
+
+module.exports = AppFeatures;
